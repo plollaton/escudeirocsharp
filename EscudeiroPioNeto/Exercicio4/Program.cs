ebfe8effe3fda40b7dd73fbcd54df536cc2c7fa1
piolollato <Plollaton@hotmail.com>
2020-08-18 15:27:43 -0300

    Final exercicios 4 e 5

---
+++ EscudeiroPioNeto/Exercicio4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercicio4
+{
+    class Aluno
+    {
+        public Double Nota { get; set; }
+
+        public String Nome { get; set; }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Aluno> alunos = new List<Aluno>();
+            Console.WriteLine("Informe o nome e as notas, para encerrar deixe o nome em branco.");
+            while (true)
+            {
+                Aluno aluno = new Aluno();
+                Console.Write("Nome do aluno: ");
+                aluno.Nome = Console.ReadLine();
+                if (aluno.Nome.Trim().Equals(""))
+                {
+                    break;
+                }
+                Console.Write("Informe a nota do aluno: ");
+                aluno.Nota = Double.Parse(Console.ReadLine());
+                alunos.Add(aluno);
+            }
+            foreach (var item in alunos)
+            {
+                if (item.Nota >= 70)
+                {
+                    Console.WriteLine($"O aluno {item.Nome} atigiu nota maior que 70.");
+                }
+            }
+        }
+    }
+}
---
+++ EscudeiroPioNeto/exercicio5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace exercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe os valores para o cálculo da formula de baskara.");
+            Console.Write("Informe o valor de A: ");
+            int a = Int32.Parse(Console.ReadLine());
+            Console.Write("Informe o valor de B: ");
+            int b = Int32.Parse(Console.ReadLine());
+            Console.Write("Informe o valor de C: ");
+            int c = Int32.Parse(Console.ReadLine());
+
+            var delta = Math.Pow(b, 2) - (4 * a * c);
+            if(delta <= 0)
+            {
+                Console.WriteLine("Não é possível calcular o valor da equação, pois o delta é negativo.");
+                return;
+            }
+
+            var x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+            var x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+
+            Console.WriteLine(delta);
+            Console.WriteLine(x1);
+            Console.WriteLine(x2);
+
+        }
+    }
+}
