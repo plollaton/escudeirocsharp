ea2450af04e45064461ee82291826236d7328d1a
piolollato <Plollaton@hotmail.com>
2020-08-18 14:38:17 -0300

    Final exercicio 3

---
+++ EscudeiroPioNeto/Exercicio3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+            int step = 1;
+            while(num < 100)
+            {
+                if(step == 3)
+                {
+                    Console.WriteLine(num);
+                    step = 1;
+                    num += 1;
+                }
+                step += 1;
+                num += 1;
+            }
+        }
+    }
+}
