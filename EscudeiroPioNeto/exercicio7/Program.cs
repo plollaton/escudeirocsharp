9546643f48c2c4e5d0cce112a6f6d566b05797a5
piolollato <Plollaton@hotmail.com>
2020-08-19 14:28:21 -0300

    Final exercicios 7 e 8

---
+++ EscudeiroPioNeto/exercicio7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace exercicio7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numeros = new List<int>();
+            int idx = 0;
+            while(idx <= 3)
+            {
+                Console.Write($"Informe o numero {idx + 1}: ");
+                try
+                {
+                    numeros.Add(int.Parse(Console.ReadLine()));
+                    idx++;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("O valor informado não é um número inteiro, informe novamente.");
+                    continue;
+                }
+            }
+            int soma = 0;
+            soma = (from x in numeros where x % 2 == 0 select x).Sum();
+
+            Console.WriteLine($"A soma dos itens pares é: {soma}.");
+        }
+    }
+}
---
+++ EscudeiroPioNeto/exercicio8/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace exercicio8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<decimal> numeros = new List<decimal>();
+            Console.WriteLine("Informe valores decimais, para ordenação, pressione <Enter> para encerrar.");
+            while (true)
+            {
+                Console.Write("Informe o valor: ");
+                try
+                {
+                    string valor = Console.ReadLine();
+                    if (valor.Trim().Equals(""))
+                    {
+                        break;
+                    }
+                    numeros.Add(Decimal.Parse(valor));
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Valor informado não pode ser convertido em decimal.");
+                }
+            }
+
+            numeros.Sort();
+            Console.WriteLine("Ordem crescente:");
+            for(int i = 0; i <= numeros.Count - 1; i++)
+            {
+                Console.WriteLine(numeros[i]);
+            }
+            Console.WriteLine("Ordem decrescente:");
+            for (int x = numeros.Count - 1; x >= 0; x--)
+            {
+                Console.WriteLine(numeros[x]);
+            }
+        }
+    }
+}
