1aff1590d5ceec5c45f018b6a6e9c90ab9286c00
piolollato <Plollaton@hotmail.com>
2020-08-21 00:09:39 -0300

    Finalizado OO_1

---
+++ EscudeiroPioNeto/OO_1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace OO_1
+{
+    class Pessoa
+    {
+        private DateTime _dataNascimento;
+        private int _idade;
+
+        public string Nome { get; set; }
+        public DateTime DataNascimento
+        {
+            get
+            {
+                return _dataNascimento;
+            }
+
+            set
+            {
+                _dataNascimento = value;
+                Idade = DateTime.Now.Year - _dataNascimento.Year;
+            }
+        }
+
+        public double Altura { get; set; }
+
+        public int Idade
+        {
+            get
+            {
+                return _idade;
+            }
+
+            private set
+            {
+                _idade = value;
+            }
+        }
+
+        public Pessoa(string nome, DateTime dataNascimento, double altura)
+        {
+            Nome = nome;
+            DataNascimento = dataNascimento;
+            Altura = altura;
+        }
+
+        public override string ToString()
+        {
+            return $"Nome: {Nome}, data de nascimento: {DataNascimento.ToShortDateString()}, altura: {Altura} e tem a idade de {Idade} anos.";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa1 = new Pessoa("Pio", new DateTime(1979, 02, 24), 1.8);
+            Console.WriteLine(pessoa1.ToString());
+        }
+    }
+}
